5d75109c42394cb164fa8dd49867adcd0d2fbab3
NickNGcc <nicholas2472@gmail.com>
2024-10-24 02:13:20 +0800

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -5,4 +5,4 @@ wget --no-check-certificate $DOWNLOAD_PREFIX/Dockerfile -O Dockerfile
 wget --no-check-certificate $DOWNLOAD_PREFIX/run.sh -O run.sh
 
 docker build -t node .
-docker run -d --name node -p 3311:3311 -p 80:80 node
+docker run -d --restart=always --name node -p 3311:3311 -p 80:80 node
