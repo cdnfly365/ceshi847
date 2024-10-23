f034fc4d3991c4310d90c00c7cf313d6d20224b5
root <24724604@qq.com>
2024-10-24 02:04:43 +0800

    change dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -4,3 +4,5 @@ export DOWNLOAD_PREFIX="http://github.itzmx.com/NickNGcc/cdnbest/main/docker"
 wget --no-check-certificate $DOWNLOAD_PREFIX/Dockerfile -O Dockerfile
 wget --no-check-certificate $DOWNLOAD_PREFIX/run.sh -O run.sh
 
+docker build -t node .
+docker run -d --name node -p 3311:3311 -p 80:80 node
