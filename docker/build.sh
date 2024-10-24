fb5c69cf53ba67d6f2bf7036841f15349fe9b795
cdnfly365 <cdnfly365@gmail.com>
2024-10-25 01:46:25 +0800

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-export DOWNLOAD_PREFIX="http://github.itzmx.com/NickNGcc/cdnbest/main/docker"
+export DOWNLOAD_PREFIX="http://github.itzmx.com/cdnfly365/cdnbest/main/docker"
 
 wget --no-check-certificate $DOWNLOAD_PREFIX/Dockerfile -O Dockerfile
 wget --no-check-certificate $DOWNLOAD_PREFIX/run.sh -O run.sh
